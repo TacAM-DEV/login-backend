5dc9ca07f975135289379ca8f9bd9e2b271f9610
Lukas <github@muffl0n.de>
2020-03-22 00:00:20 +0100

    Login Route

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const Koa = require('koa');
 const Router = require('@koa/router');
 
+let login = require('./src/login');
+
 const app = new Koa();
 const router = new Router();
 
 router.get('/', (ctx, next) => {
-  ctx.body = "Nothing to see here ¯\_(ツ)_/¯";
+  ctx.body = "Nothing to see here ¯\\_(ツ)_/¯";
 });
+
+router.get('/signup', (ctx, next) => {
+    ctx.body = login.signup;
+})
+
+router.get('/signin', (ctx, next) => {
+    ctx.body = login.signin;
+})
 
 app
   .use(router.routes())
   .use(router.allowedMethods());
 
-app.listen(1337);
+app.listen(1338);
